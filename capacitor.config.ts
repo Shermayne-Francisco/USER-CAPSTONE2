3d8324feb01373ebd69b4e47e2c4aecf7e682259
Shermayne-Francisco <201911338@gordoncollege.edu.ph>
2023-01-06 17:40:52 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PetHealth',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
