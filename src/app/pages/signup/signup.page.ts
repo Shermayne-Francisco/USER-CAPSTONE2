cd0a4534b237e8c4128437be0d9b5ee449ad40f5
Shermayne-Francisco <201911338@gordoncollege.edu.ph>
2023-01-08 02:20:37 +0800

    scroll

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-signup',
+//   templateUrl: './signup.page.html',
+//   styleUrls: ['./signup.page.scss'],
+// })
+// export class SignupPage implements OnInit {
+//   private dateValue: any;
+
+//   constructor() {}
+//   ngOnInit(): void {
+//     throw new Error('Method not implemented.');
+//   }
+
+//   get date(): any {
+//     return this.dateValue;
+//   }
+//   set date(value: any) {
+//     console.log({ value });
+//     this.dateValue = value;
+//   }
+// }
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class SignupPage {
+  private dateValue: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  get date(): any {
+    return this.dateValue;
   }
-
+  set date(value: any) {
+    console.log({ value });
+    this.dateValue = value;
+  }
 }
