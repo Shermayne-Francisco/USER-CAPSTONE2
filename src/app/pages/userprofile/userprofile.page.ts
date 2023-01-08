4f716d85891ff463a753e3615d916309a7756563
Shermayne-Francisco <201911338@gordoncollege.edu.ph>
2023-01-09 02:44:55 +0800

    user

--- src/app/pages/userprofile/userprofile.page.ts
+++ src/app/pages/userprofile/userprofile.page.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-userprofile',
+//   templateUrl: './userprofile.page.html',
+//   styleUrls: ['./userprofile.page.scss'],
+// })
+// export class UserprofilePage implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-userprofile',
-  templateUrl: './userprofile.page.html',
-  styleUrls: ['./userprofile.page.scss'],
+  templateUrl: 'userprofile.page.html',
+  styleUrls: ['userprofile.page.scss'],
 })
-export class UserprofilePage implements OnInit {
+export class UserprofilePage {
+  account = 'details';
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
 }
